20a22e49dbb02f220deaaf2f565e39e94908da46
Inje Kim <43752470+Kiminje@users.noreply.github.com>
2025-03-07 10:52:00 -0800

    Enable off-path prediction (#195)
    **Usage**
    Add '--spec_level 3' to your json file
    
    **Task**
    Now off-path prediction performs actual prediction instead of returning default dir.
    
    **Results**
    Since off-path prediction modifies global history in bp.c:477(bp_data->global_hist), it changes behavior of ibtb index(ibtb uses global history to generate index) in bp_targ_mech.c:313
    **Application**		| **instructions**	| **cycles**		| **IPC**
    mongodb			| 60087301468		| 35724425216	| 1.681966921
    gcc				| 33924758941		| 22840291425	| 1.485303244
    verilator		| 3.94535E+11		| 6.8169E+11	| 0.57876021
    xgboost			| 1.14884E+11		| 2.00609E+12	| 0.057267305
    clang			| 23517834114		| 13320322235	| 1.76556045

--- src/bp/cbp_to_scarab.cc
+++ src/bp/cbp_to_scarab.cc
@@ -86,7 +86,8 @@ template <>
 uns8 CBP_To_Scarab_Intf<TAGE64K>::pred(Op* op) {
   uns proc_id = op->proc_id;
   if (op->off_path)
-    return op->oracle_info.dir;
+    if (SPEC_LEVEL < BP_PRED_ONOFF_SPEC_UPDATE_S_ONOFF_N_ON)
+      return op->oracle_info.dir;
   uns8 pred = cbp_predictors.at(proc_id).GetPrediction(op->inst_info->addr, &op->bp_confidence, op);
 
   return pred;
